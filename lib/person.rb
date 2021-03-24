76867a7ec30d408dce750d1e9da8d49e97d2abb8
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-24 12:46:53 -0400

    commit tree 998016e652b6fffe2404f8cc8588621d54721520

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
-  #your code here
+
+  #will assign an arbitrary number of properties on initialization
+  #Each key in the attributes hash will become a property of an initialized Person instance. 
+  #So, you should make an attr_accessor for each of the above properties
+  attr_accessor :name, :birthday, :hair_color, :eye_color, 
+  :height, :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  #will also assign an arbitrary number of properties on initialization
+  #Your initialize method should use iteration and the .send method 
+  #to mass assign the value of each key/value pair to its associated key (i.e. method).
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
 end
